f24ced1fb8fd29941a5ac294f6dcabcf9f0b7443
Deep Kandpal <deepkandpal16@gmail.com>
2018-11-25 07:06:16 -0800

    admin message

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+print("******************************************************************")
 print("Hello World , This is my First Example")
+name = input("Enter the Name:")
+if name == "deep":
+	print("You are the admin")
+else:
+	print("Please check your rights")
